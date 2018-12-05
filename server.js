4350c47b4cd3a25372842696a5f1a1a6b7554cd2
Austin.Wiedenman <awiedenman@gmail.com>
2018-12-04 17:27:53 -0700

    Set up express GET route handlers for palettes and projects

---
+++ server.js
@@ -0,0 +1,35 @@
+const express = require('express');
+const app = express();
+const environment = process.env.NODE_ENV || 'development';
+const configuration = require('./knexfile')[environment];
+const database = require('knex')(configuration);
+
+app.set('port', process.env.PORT || 3000);
+
+app.listen(app.get('port'), () => {
+  console.log(`${app.locals.title} is running on ${app.get('port')}.`);
+});
+
+app.get('/', (request, response) => {
+  response.send('Palette Picker 2');
+});
+
+app.get('/api/v1/projects', (request, response) => {
+  database('projects').select()
+  .then((projects) => {
+    response.status(200).json(projects);
+  })
+  .catch((error) => {
+    response.status(500).json({error})
+  })
+});
+
+app.get('/api/v1/palettes', (request, response) => {
+  database('palettes').select()
+  .then((palettes) => {
+    response.status(200).json(palettes)
+  })
+  .catch((error) => {
+    response.status(500).json({error})
+    })
+})
